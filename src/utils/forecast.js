5700690e48275ca854c47e6d56133f288fac46f1
K Olfato <karenolfato@gmail.com>
2021-08-03 06:58:09 +0800

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,27 @@
+const request = require("request");
+
+const forecast = (latitude, longitude, callback) => {
+  const url =
+    "http://api.weatherstack.com/current?access_key=f78d3c936637dc502c59c9fd38958d0d&query=" +
+    longitude +
+    "," +
+    latitude +
+    "&units=f";
+
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback("Unable to connect to weather services!", undefined);
+    } else if (body.error) {
+      console.log(body.error);
+      callback("Unable to find location. Try again.", undefined);
+    } else {
+      callback(
+        undefined,
+        body.current.weather_descriptions[0] +
+          `. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out.`
+      );
+    }
+  });
+};
+
+module.exports = forecast;
