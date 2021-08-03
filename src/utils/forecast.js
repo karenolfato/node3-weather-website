4f1f15ad46ae40ae1a4d4c9f50b4863f9ec90b87
K Olfato <karenolfato@gmail.com>
2021-08-03 12:12:42 +0800

    Add humidity to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -18,7 +18,7 @@ const forecast = (latitude, longitude, callback) => {
       callback(
         undefined,
         body.current.weather_descriptions[0] +
-          `. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out.`
+          `. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out. The humidity is ${body.current.humidity}%.`
       );
     }
   });
